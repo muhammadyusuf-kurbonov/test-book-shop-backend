97770337b8d3985c88d9d2b1bb454da6d35f8435
Muhammadyusuf Kurbonov <muhammadyusuf.kurbonov2002@gmail.com>
2024-10-24 10:54:55 +0500

    initial: install prisma initial: implement auth

---
+++ src/.env.d.ts
@@ -0,0 +1,13 @@
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            PORT: number;
+            DATABASE_URL: string;
+            SECRET: string;
+        }
+    }
+}
+
+// If this file has no import/export statements (i.e. is a script)
+// convert it into a module by adding an empty export statement.
+export {};
---
+++ src/auth/auth.dto.ts
@@ -0,0 +1,6 @@
+export type SignupDTO = {
+    email: string;
+    password: string;
+    name: string;
+    pseudonym: string;
+}
